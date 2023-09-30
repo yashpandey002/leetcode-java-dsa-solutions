320539d016a3c50478a888b35c44f0fe34f790a2
Yash Pandey <pandeyyash002@gmail.com>
2023-09-30 17:42:00 +0530

    Solved Matrix diagonal sum problem

---
+++ matrix-diagonal-sum/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int sum = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i];
+            if (i != mat.length - 1 - i) {
+                sum += mat[i][mat.length - 1 - i];
+            }
+        }
+
+        System.out.println("Sum is: " + sum);
+    }
+}
